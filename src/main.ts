fcf116ed404f02a380156973aa5df32faa4c1337
fmanuel98 <faugf320@gmail.com>
2021-09-03 12:49:41 +0100

    first commit

---
+++ src/main.ts
@@ -0,0 +1,18 @@
+import { createApp } from "vue";
+import App from "./App.vue";
+import PrimeVue from "primevue/config";
+import router from "./router";
+import store from "./store";
+import InputText from "primevue/inputtext";
+import Button from "primevue/button";
+
+import "primevue/resources/themes/saga-blue/theme.css";
+import "primevue/resources/primevue.min.css";
+import "primeicons/primeicons.css";
+const app = createApp(App);
+app.use(PrimeVue);
+app.component("InputText", InputText);
+app.component("Button", Button);
+app.use(store);
+app.use(router);
+app.mount("#app");
