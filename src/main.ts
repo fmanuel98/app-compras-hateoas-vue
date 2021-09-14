ef3ac7e73bf7de4fbe7549e0da7c7a5d3ad20200
fmanuel98 <faugf320@gmail.com>
2021-09-14 22:56:38 +0100

    feat: add modules

--- src/main.ts
+++ src/main.ts
@@ -1,14 +1,16 @@
 import { createApp } from "vue";
-import App from "./App.vue";
 import PrimeVue from "primevue/config";
-import router from "./router";
-import store from "./store";
+
 import InputText from "primevue/inputtext";
 import Button from "primevue/button";
-
 import "primevue/resources/themes/saga-blue/theme.css";
 import "primevue/resources/primevue.min.css";
 import "primeicons/primeicons.css";
+
+import store from "./store";
+import router from "./router";
+import App from "./App.vue";
+
 const app = createApp(App);
 app.use(PrimeVue);
 app.component("InputText", InputText);
